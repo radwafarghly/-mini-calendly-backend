f5e65caf80f626c6196d305aed2f02835a26ce40
radwafarghly <radwa.a.farghly@gmail.com>
2023-01-21 18:00:24 +0200

    Update Expire token of auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,7 +32,7 @@ class AuthController extends Controller
         return [
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => $this->guard()->factory()->getTTL() * 122222222
+            'expires_in' => $this->guard()->factory()->getTTL(30) * 180
         ];
     }
     /**
